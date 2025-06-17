dabf3c445b449d082780f92b3f42c846427f532c
Eliseu <eliseumatos321@gmail.com>
2025-06-17 12:42:46 -0300

    comentei e coloquei apenas return

--- estudo/routes.py
+++ estudo/routes.py
@@ -3,13 +3,13 @@ from flask import render_template
 #pip install flask
 @app.route('/', methods=["GET","POST"])
 def homepage():
-    usuario='eliseu'
-    idade=24
-    estado_civil='solteiro'
+      return render_template('index.html')
+    # usuario="eliseu"
+    # idade=24
+    # estado_civil="solteiro"
 
-    dici={
-        'usuario': usuario,
-        'idade':idade,
-        'estado_civil':estado_civil
-     }
-    return render_template('index.html', dici=dici)
+    # dici={
+    #     'usuario': usuario,
+    #     'idade':idade,
+    #     'estado_civil':estado_civil
+    #  }
