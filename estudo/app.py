a0a4ce5f7c4af8732fac8163f6b341300e297e30
Eliseu <eliseumatos321@gmail.com>
2025-06-18 11:52:26 -0300

    cometei pois estava atrapalhando

--- estudo/app.py
+++ estudo/app.py
@@ -1,87 +1,35 @@
-# #pip install Flask and pip install mercadopago no cmd
-# #arquivo não poder conter o nome como flask,pois, quando for import ira da erro
+# # #pip install Flask and pip install mercadopago no cmd
+# # #arquivo não poder conter o nome como flask,pois, quando for import ira da erro
+# # from flask import Flask, render_template
+# # from apimercadopago import gerar_link_pagamento
+
+
 # from flask import Flask, render_template
-# from apimercadopago import gerar_link_pagamento
+# from apimercadopago import gerar_link_pagamento  # Verifique o nome!
 
-# app=Flask(__name__)
+# # #para roda o cors e da seguimento ao pagamento
+# # from flask import Flask, request, jsonify
+# # from flask_cors import CORS
+# # import mercadopago
 
-# @app.route("/homepage")
-# def  homepage():
-#   # link_iniciar_pagamento = gerar_link_pagamento()
-#   # return render_template("homepage.html", link_pagamento=link_iniciar_pagamento)
-#   return render_template("homepage.html")
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     link_pagamento = gerar_link_pagamento()
+#     return render_template("home.html", link_pagamento=link_pagamento)
 
 # @app.route("/compracerta")
-# def  compra_certa():
-#   return render_template("compracerta.html")
+# def compra_certa():
+#     return render_template("compracerta.html")
 
 # @app.route("/compraerrada")
 # def compra_errada():
-#   return render_template("compraerrada.html")
-
-# # if __name__ == "__main__":
-# #  app.run()
-# if __name__ == "__main__":
-#  app.run(host="127.0.0.1", port=5000, debug=True)
-
-
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def homepage():
-#     return "Olá, Flask está funcionando!"
+#     return render_template("compraerrada.html")
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
 
-#codigo funcionou
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route("/")
-# def homepage():
-#     return "Rota principal funcionando!"
-
-# @app.route("/compracerta")
-# def compracerta():
-#     return "Compra certa funcionando!"
-
-# @app.route("/compraerrada")
-# def compraerrada():
-#     return "Compra errada funcionando!"
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-from flask import Flask, render_template
-from apimercadopago import gerar_link_pagamento  # Verifique o nome!
-
-# #para roda o cors e da seguimento ao pagamento
-# from flask import Flask, request, jsonify
-# from flask_cors import CORS
-# import mercadopago
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    link_pagamento = gerar_link_pagamento()
-    return render_template("home.html", link_pagamento=link_pagamento)
-
-@app.route("/compracerta")
-def compra_certa():
-    return render_template("compracerta.html")
-
-@app.route("/compraerrada")
-def compra_errada():
-    return render_template("compraerrada.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-#fazer cadastro no ngrok e configurar o restante do codigo do mercado pago
+# #fazer cadastro no ngrok e configurar o restante do codigo do mercado pago
 
 
