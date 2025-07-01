df0b231627b8e55671059ee88b355fbdab560fbc
Eliseu <eliseumatos321@gmail.com>
2025-07-01 12:33:41 -0300

    add routes

--- estudo/routes.py
+++ estudo/routes.py
@@ -3,7 +3,6 @@ from estudo import app
 from flask import render_template, url_for 
 # inicio da nosso controle de login.
 # pip install flask-login flask-bcrypt 
-from flask
 @app.route("/", methods=["GET","POST"])
 def homepage():
       return render_template('index.html')
