2f407d23406b7607394169db8ca6125b14f66d8a
Eliseu <eliseumatos321@gmail.com>
2025-07-07 11:02:32 -0300

    fazendo mudanças

---
+++ estudo/roustes1.py
@@ -0,0 +1,21 @@
+#pip install flask
+from estudo import app
+from flask import render_template, url_for 
+# inicio da nosso controle de login.
+# pip install flask-login flask-bcrypt 
+from flask
+@app.route("/", methods=["GET","POST"])
+def homepage():
+      return render_template('index.html')
+    # usuario="eliseu"
+    # idade=24
+    # estado_civil="solteiro"
+
+    # dici={
+    #     'usuario': usuario,
+    #     'idade':idade,
+    #     'estado_civil':estado_civil
+    #  }
+poruqe as coisa não estão aparecendo?
+# teste de modificação
+
