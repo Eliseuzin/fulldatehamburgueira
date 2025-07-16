ab8777a1301684c2098a9344a6d7c355e0246a73
Eliseu <eliseumatos321@gmail.com>
2025-07-16 11:15:04 -0300

    estou cirando novamente a rota

--- estudo/roustes1.py
+++ estudo/roustes1.py
@@ -3,7 +3,6 @@ from estudo import app
 from flask import render_template, url_for 
 # inicio da nosso controle de login.
 # pip install flask-login flask-bcrypt 
-from flask
 @app.route("/", methods=["GET","POST"])
 def homepage():
       return render_template('index.html')
@@ -16,6 +15,9 @@ def homepage():
     #     'idade':idade,
     #     'estado_civil':estado_civil
     #  }
-poruqe as coisa não estão aparecendo?
 # teste de modificação
+
+@app.route("cadastro/", methods=["GET","POST"])
+def cadastro():
+      return render_template('cadastro.html')
 
