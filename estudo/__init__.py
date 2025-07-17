7fd166adff017880f740c6ec39b194c43ec0ce32
Eliseu <eliseumatos321@gmail.com>
2025-07-17 12:48:08 -0300

    mudei o login-manager, pois estava dando uma rota circular

--- estudo/__init__.py
+++ estudo/__init__.py
@@ -41,12 +41,13 @@ app=Flask(__name__)
 
 #configurar o banco de dados
 #quando criamos o banco de dados, ele chamará database.db
-app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URI')
+# app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database.db'
 #aumenta a prioridade, e evita o checking
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 #mais para frente aprenderemos a deixa o nosso banco de dados mais seguro, sem deixa chaves de acesso livres
-app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
-
+# app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
+app.config['SECRET_KEY']='8a97b281fcaf00eaee650eb9078eded3d3c0656406f7d8b103191d1fb7f3ef18598a55ae52791310f2e96b716d17c78de9e1'
 db=SQLAlchemy(app)
 migrate=Migrate(app, db)
 #comando para criar o banco de dados
@@ -54,12 +55,19 @@ migrate=Migrate(app, db)
 #é necessário apenas um banco de dados por projeto.
 
 
+#inicio do controle de login
+from estudo.models import User
+# importa as rotas
+from estudo.routes import homepage
+
 login_manager=LoginManager(app)
 bcrypt=Bcrypt(app)
 # controle de login
 login_manager.login_view='login'
 
+# significa que você está usando o Flask-Login, mas não definiu a função obrigatória user_loader — que é necessária para o login funcionar corretamente.
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
-# importa as rotas
-from estudo.routes import homepage
 
