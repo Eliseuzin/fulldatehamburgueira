66a7d587eb37fa1150a7db79392c0f6ba3c92f37
Eliseu <eliseumatos321@gmail.com>
2025-07-18 13:51:45 -0300

    add o controle de login da loja

--- estudo/models.py
+++ estudo/models.py
@@ -2,7 +2,7 @@ from estudo import db
 from datetime import datetime
 from flask_login import UserMixin
 
-#inicio do controle de login
+#inicio do controle de login usuario
 # significa que você está usando o Flask-Login, mas não definiu a função obrigatória user_loader — que é necessária para o login funcionar corretamente.
 
 class User(db.Model,UserMixin):
@@ -12,4 +12,16 @@ class User(db.Model,UserMixin):
     sobrenome=db.Column(db.String, nullable=True)
     email=db.Column(db.String, nullable=True)
     senha=db.Column(db.String, nullable=True)
- #fim do controle de login
+ #fim do controle de login usuario
+
+ #inicio do controle de login loja
+class Store(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    nome=db.Column(db.String, nullable=True)
+    sobrenome=db.Column(db.String, nullable=True)
+    email=db.Column(db.String, nullable=True)
+    senha=db.Column(db.String, nullable=True)
+    cnpj=db.Column(db.Integer, nullable=True)
+    telefone=db.Column(db.Integer, nullable=True)
+    endereço=db.Column(db.String, nullable=True)
+ #fim do controle de login loja
