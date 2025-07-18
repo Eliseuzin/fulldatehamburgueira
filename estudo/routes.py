0022163924634bf5c733817f32460cb42ae13383
Eliseu <eliseumatos321@gmail.com>
2025-07-18 13:52:05 -0300

    mudei o nome StoreForm

--- estudo/routes.py
+++ estudo/routes.py
@@ -4,7 +4,7 @@ from flask import render_template, url_for
 from flask import redirect,flash
 from flask_login import login_user, logout_user, current_user
 
-from estudo.forms import UserForm
+from estudo.forms import UserForm, StoreForm
 
 
 @app.route("/", methods=["GET","POST"])
@@ -12,14 +12,28 @@ def homepage():
       return render_template('index.html')
 
 
-@app.route('/cadastro/',methods=["GET","POST"])
-def cadastro():
+@app.route('/cadastrousuario/',methods=["GET","POST"])
+def cadastrousuario():
     form=UserForm()
     if form.validate_on_submit():
         user=form.save()
         login_user(user, remember=True)
-        flash('Cadastro realizado com sucesso! Você já logado. ','success')
+        flash('Cadastro realizado com sucesso! Você já está logado. ','success')
         return redirect(url_for('homepage'))
 
 
-    return render_template('cadastro.html', form=form)
+    return render_template('cadastrousuario.html', form=form)
+
+@app.route('/cadastroloja/', methods=["GET","POST"])
+def cadastroloja():
+     form=StoreForm()
+     if form.validate_on_submit():
+         loja=form.save()
+         login_user(loja, remember=True)
+         flash('Cadastro realizado com sucesso! Você já está logado', 'success')
+         return redirect(url_for('homepage'))
+     
+     
+     return render_template('cadastroloja.html', form=form)
+        
+
