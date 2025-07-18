1036586e7063471e8ab8ee239b716ad0340246ff
Eliseu <eliseumatos321@gmail.com>
2025-07-18 13:51:29 -0300

    add a verificação de inteiro

--- estudo/forms.py
+++ estudo/forms.py
@@ -1,7 +1,7 @@
 #agora, iremos criar uma aplicaçao mais segura sobre o formulario e banco de dados
 #1- instalar pip install flask_wtf
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField,IntegerField
 # EqualTo verifica se um campo é == ao outro
 # para a validaçao do email precisamos instalar pip install email_validator
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
@@ -11,5 +11,16 @@ class UserForm(FlaskForm):
     sobrenome=StringField('Sobrenome', validators=[DataRequired()])
     email=StringField('Email', validators=[DataRequired()])
     senha=PasswordField('Senha', validators=[DataRequired()])
-    confirmarsenha=PasswordField('Confirmar_senha', validators=[DataRequired(), EqualTo('senha')])
-    btnSubmit=SubmitField('Cadastrar')
+    confirmarsenha=PasswordField('Confirmar senha', validators=[DataRequired(), EqualTo('senha')])
+    btnSubmit=SubmitField('Cadastrar usuário')
+
+class StoreForm(FlaskForm):
+    nome=StringField('Nome', validators=[DataRequired()])
+    sobrenome=StringField('Sobrenome', validators=[DataRequired()])
+    email=StringField('Email', validators=[DataRequired()])
+    senha=PasswordField('Senha', validators=[DataRequired()])
+    confirmarsenha=PasswordField('Confirmar senha', validators=[DataRequired()])
+    cnpj=IntegerField('CNPJ', validators=[DataRequired()])
+    telefone=IntegerField('Telefone', validators=[DataRequired()])
+    endereço=StringField('Endereço', validators=[DataRequired()])
+    btnSubmit=SubmitField('Cadastrar loja')
