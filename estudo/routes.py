1e9e94e9d081d46ea16d39eca1dc52799a0105e8
Eliseu <eliseumatos321@gmail.com>
2025-09-22 12:49:15 -0300

    criando novas rotas para início e login

--- estudo/routes.py
+++ estudo/routes.py
@@ -33,6 +33,11 @@ def homepage():
     # else:
         return render_template('index.html')
 
+#rota para página inicial em todas as páginas
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
 
 
 @app.route('/login/', methods=["GET", "POST"])
